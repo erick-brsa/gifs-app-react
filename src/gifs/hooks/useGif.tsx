573f0e6bdedbdce3bfc52e97fbf7d24326415af9
Erick Briones <erickalan518@gmail.com>
2026-08-23 23:00:29 -0600

    Refactor GIF data handling: update import paths and implement caching mechanism

--- src/gifs/hooks/useGif.tsx
+++ src/gifs/hooks/useGif.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { getGifsByQuery } from '../actions/get-gifs-by-query.action';
 import type { Gif } from '../interfaces/gif';
+
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGif = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleTermClicked = async (term: string) => {
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return
+    };
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
   };
@@ -20,6 +28,7 @@ export const useGif = () => {
       ...previousTerms.slice(0, 7),
     ]);
     const gifs = await getGifsByQuery(query);
+    gifsCache.current[query] = gifs;
     setGifs(gifs);
   };
   return {
